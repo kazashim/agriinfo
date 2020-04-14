a50fe749d73b17c9501a811ceb118197d505a69f
kazashim <kazashimkuzasuwat@gmail.com>
2020-04-14 17:38:34 +0200

    update

--- login.php
+++ login.php
@@ -107,7 +107,7 @@
                                                         </div>
                                                         <div class="text-right"><a href="auth-forgot-password.html" class="card-link">Forgot Password?</a></div>
                                                     </div>
-                                                    <a href="auth-register.html" class="btn btn-outline-primary float-left btn-inline">Register</a>
+                                                    <a href="register.php" class="btn btn-outline-primary float-left btn-inline">Register</a>
                                                     <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
                                                 </form>
                                             </div>
