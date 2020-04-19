e7253607261817e2ad7c31e47b36093f3c258085
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-04-20 00:14:29 +0200

    update

--- login.php
+++ login.php
@@ -117,147 +117,125 @@ if(isset($_POST["login"])){
 
 
 <!DOCTYPE html>
-
-<!-- BEGIN: Head-->
-
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
-    <title>Login Page - AgriInfo</title>
-    <link rel="apple-touch-icon" href="vendor/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="vendor/app-assets/images/ico/favicon.ico">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
-
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/vendors/css/vendors.min.css">
-    <!-- END: Vendor CSS-->
-
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/themes/semi-dark-layout.css">
-
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/core/colors/palette-gradient.css">
-    <link rel="stylesheet" type="text/css" href="vendor/app-assets/css/pages/authentication.css">
-    <!-- END: Page CSS-->
-
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="vendor/assets/css/style.css">
-    <!-- END: Custom CSS-->
-
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title>Dashtreme - Multipurpose Bootstrap4 Admin Template</title>
+  <!--favicon-->
+  <link rel="icon" href="vendor/images/favicon.ico" type="image/x-icon">
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/css/bootstrap.min.css" rel="stylesheet"/>
+  <!-- animate CSS-->
+  <link href="vendor/css/animate.css" rel="stylesheet" type="text/css"/>
+  <!-- Icons CSS-->
+  <link href="vendor/css/icons.css" rel="stylesheet" type="text/css"/>
+  <!-- Custom Style-->
+  <link href="vendor/css/app-style.css" rel="stylesheet"/>
+  
 </head>
-<!-- END: Head-->
 
-<!-- BEGIN: Body-->
+<body>
 
-<body class="vertical-layout vertical-menu-modern dark-layout 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="click" data-menu="vertical-menu-modern" data-col="1-column" data-layout="dark-layout">
-    <!-- BEGIN: Content-->
-    <div class="app-content content">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
-            <div class="content-header row">
-            </div>
-            <div class="content-body">
-                <section class="row flexbox-container">
-                    <div class="col-xl-8 col-11 d-flex justify-content-center">
-                        <div class="card bg-authentication rounded-0 mb-0">
-                            <div class="row m-0">
-                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
-                                    <img src="vendor/app-assets/images/pages/login.png" alt="branding logo">
-                                </div>
-                                <div class="col-lg-6 col-12 p-0">
-                                    <div class="card rounded-0 mb-0 px-2">
-                                        <div class="card-header pb-1">
-                                            <div class="card-title">
-                                                <h4 class="mb-0">Login</h4>
-                                            </div>
-                                        </div>
-                                        <p class="px-2">Welcome back, please login to your account.</p>
-                                        <div class="card-content">
-                                            <div class="card-body pt-1">
-                                                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST" >
-                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
-                                                        <input type="text" class="form-control" name="username" id="user-name" value="<?php echo @$uname; ?>" placeholder="Username" required>
-                                                        <div class="form-control-position">
-                                                            <i class="feather icon-user"></i>
-                                                        </div>
-                                                        <label for="user-name">Username</label>
-                                                    </fieldset>
+<!-- start loader -->
+   <div id="pageloader-overlay" class="visible incoming"><div class="loader-wrapper-outer"><div class="loader-wrapper-inner" ><div class="loader"></div></div></div></div>
+   <!-- end loader -->
 
-                                                    <fieldset class="form-label-group position-relative has-icon-left">
-                                                        <input type="password" class="form-control"  name="password" id="user-password" placeholder="Password" required>
-                                                        <div class="form-control-position">
-                                                            <i class="feather icon-lock"></i>
-                                                        </div>
-                                                        <label for="user-password">Password</label>
-                                                    </fieldset>
-                                                    <div class="form-group d-flex justify-content-between align-items-center">
-                                                        <div class="text-left">
-                                                            <fieldset class="checkbox">
-                                                                <div class="vs-checkbox-con vs-checkbox-primary">
-                                                                    <input type="checkbox">
-                                                                    <span class="vs-checkbox">
-                                                                        <span class="vs-checkbox--check">
-                                                                            <i class="vs-icon feather icon-check"></i>
-                                                                        </span>
-                                                                    </span>
-                                                                    <span class="">Remember me</span>
-                                                                </div>
-                                                            </fieldset>
-                                                        </div>
-                                                        <div class="text-right"><a href="auth-forgot-password.html" class="card-link">Forgot Password?</a></div>
-                                                    </div>
-                                                    <a href="register.php" class="btn btn-outline-primary float-left btn-inline">Register</a>
-                                                    <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                        <div class="login-footer">
-                                            <div class="divider">
-                                                <div class="divider-text">OR</div>
-                                            </div>
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+<!-- Start wrapper-->
+ <div id="wrapper">
 
-            </div>
-        </div>
-    </div>
-    <!-- END: Content-->
+	   <div class="card-authentication2 mx-auto my-5">
+	    <div class="card-group">
+	    	<div class="card mb-0">
+	    	   <div class="bg-signin2"></div>
+	    		<div class="card-img-overlay rounded-left my-5">
+                 <h2 class="text-white">Lorem</h2>
+                 <h1 class="text-white">Ipsum Dolor</h1>
+                 <p class="card-text text-white pt-3">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+             </div>
+	    	</div>
 
+	    	<div class="card mb-0 ">
+	    		<div class="card-body">
+	    			<div class="card-content p-3">
+	    				<div class="text-center">
+					 		<img src="vendor/images/logo-icon.png" alt="logo icon">
+					 	</div>
+					 <div class="card-title text-uppercase text-center py-3">Sign In</div>
+					   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST">
+						  <div class="form-group">
+						   <div class="position-relative has-icon-left">
+							   <label for="exampleInputUsername" class="sr-only">Username</label>
+								 <input type="text" id="exampleInputUsername" class="form-control" name="username" aria-describedby="emailHelp" value="<?php echo @$uname; ?>" placeholder="Username">
+								 <div class="form-control-position">
+									<i class="icon-user"></i>
+								</div>
+						   </div>
+						  </div>
+						  <div class="form-group">
+						   <div class="position-relative has-icon-left">
+							  <label for="exampleInputPassword" class="sr-only">Password</label>
+							  <input type="password" id="exampleInputPassword" name="password" class="form-control" placeholder="Password">
+							  <div class="form-control-position">
+								  <i class="icon-lock"></i>
+							  </div>
+						   </div>
+						  </div>
+						  <div class="form-row mr-0 ml-0">
+						  <div class="form-group col-6">
+							  <div class="icheck-material-primary">
+				               <input type="checkbox" id="user-checkbox" checked="" />
+				               <label for="user-checkbox">Remember me</label>
+							 </div>
+							</div>
+							<div class="form-group col-6 text-right">
+							 <a href="authentication-reset-password2.html">Reset Password</a>
+							</div>
+						</div>
+						<button type="button" class="btn btn-primary btn-block waves-effect waves-light">Sign In</button>
+						 <div class="text-center pt-3">
+						<p>or Sign in with</p>
 
-    <!-- BEGIN: Vendor JS-->
-    <script src="vendor/app-assets/vendors/js/vendors.min.js"></script>
-    <!-- BEGIN Vendor JS-->
+						<div class="form-row mt-4">
+						  <div class="form-group mb-0 col-6">
+						   <button type="button" class="btn bg-facebook text-white btn-block"><i class="fa fa-facebook-square"></i> Facebook</button>
+						 </div>
+						 <div class="form-group mb-0 col-6 text-right">
+						  <button type="button" class="btn bg-twitter text-white btn-block"><i class="fa fa-twitter-square"></i> Twitter</button>
+						 </div>
+						</div>
 
-    <!-- BEGIN: Page Vendor JS-->
-    <!-- END: Page Vendor JS-->
-
-    <!-- BEGIN: Theme JS-->
-    <script src="vendor/app-assets/js/core/app-menu.js"></script>
-    <script src="vendor/app-assets/js/core/app.js"></script>
-    <script src="vendor/app-assets/js/scripts/components.js"></script>
-    <!-- END: Theme JS-->
-
-    <!-- BEGIN: Page JS-->
-    <!-- END: Page JS-->
-
+						<hr>
+						<p class="">Do not have an account? <a href="authentication-signup2.html"> Sign Up here</a></p>
+						</div>
+					</form>
+				 </div>
+				</div>
+	    	</div>
+	     </div>
+	    </div>
+    
+     <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+	
+	
+	
+	</div><!--wrapper-->
+	
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/js/jquery.min.js"></script>
+  <script src="vendor/js/popper.min.js"></script>
+  <script src="vendor/js/bootstrap.min.js"></script>
+	
+  <!-- sidebar-menu js -->
+  <script src="vendor/js/sidebar-menu.js"></script>
+  
+  <!-- Custom scripts -->
+  <script src="vendor/js/app-script.js"></script>
+  
 </body>
-<!-- END: Body-->
-
 </html>
