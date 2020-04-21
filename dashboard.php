f4ee15603ca9ae7c56c37a4ec15103b05dd7afa5
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-04-21 02:03:52 +0200

    update

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,7 @@ if($_SESSION['username'] != "Admin"){
 }
  ?>
 
+<?php include "cynconnet/config.php";?>
 
 <div class="clearfix"></div>
 	
@@ -136,92 +137,3 @@ if($_SESSION['username'] != "Admin"){
          </div>
 	   </div>
 	 </div><!--End Row-->
-<!--start overlay-->
-	  <div class="overlay toggle-menu"></div>
-	<!--end overlay-->
-
-    </div>
-    <!-- End container-fluid-->
-
-   </div><!--End content-wrapper-->
-   <!--Start Back To Top Button-->
-    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
-    <!--End Back To Top Button-->
-	
-	<!--Start footer-->
-	<footer class="footer">
-      <div class="container">
-        <div class="text-center">
-          Copyright © 2019 cynojine
-        </div>
-      </div>
-    </footer>
-	<!--End footer-->
-	
-	<!--start color switcher-->
-   <div class="right-sidebar">
-    <div class="switcher-icon">
-      <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
-    </div>
-    <div class="right-sidebar-content">
-	
-	
-	 <p class="mb-0">Header Colors</p>
-      <hr>
-	  
-	  <div class="mb-3">
-	    <button type="button" id="default-header" class="btn btn-outline-primary">Default Header</button>
-	  </div>
-      
-      <ul class="switcher">
-        <li id="header1"></li>
-        <li id="header2"></li>
-        <li id="header3"></li>
-        <li id="header4"></li>
-        <li id="header5"></li>
-        <li id="header6"></li>
-      </ul>
-
-      <p class="mb-0">Sidebar Colors</p>
-      <hr>
-	  
-      <div class="mb-3">
-	    <button type="button" id="default-sidebar" class="btn btn-outline-primary">Default Sidebar</button>
-	  </div>
-	  
-      <ul class="switcher">
-        <li id="theme1"></li>
-        <li id="theme2"></li>
-        <li id="theme3"></li>
-        <li id="theme4"></li>
-        <li id="theme5"></li>
-        <li id="theme6"></li>
-      </ul>
-      
-     </div>
-   </div>
-  <!--end color switcher-->
-   
-  </div><!--End wrapper-->
-  
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/js/jquery.min.js"></script>
-  <script src="vendor/js/popper.min.js"></script>
-  <script src="vendor/js/bootstrap.min.js"></script>
-	
-  <!-- simplebar js -->
-  <script src="vendor/plugins/simplebar/js/simplebar.js"></script>
-  <!-- sidebar-menu js -->
-  <script src="vendor/js/sidebar-menu.js"></script>
-  
-  <!-- Custom scripts -->
-  <script src="vendor/js/app-script.js"></script>
-
-  <!-- Chart js -->
-  <script src="vendor/plugins/Chart.js/Chart.min.js"></script>
-  <!-- Index2 js -->
-  <script src="vendor/js/dashboard-property-listing.js"></script>
-
-</body>
-</html>
